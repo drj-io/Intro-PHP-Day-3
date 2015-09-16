112946354f3c64b539719f7c9043b1758a4cba2e
David R. Jones <david.jones@madwiremedia.com>
2015-09-15 19:18:46 -0600

    basic get example

--- get/index.php
+++ get/index.php
@@ -1,8 +1,11 @@
 <?php
+require_once('views/header.php');
 
 if(isset($_GET['name'])){
-
+  include_once('views/partials/hello.php');
 }
 else {
-
+  include_once('views/partials/input.php');
 }
+
+?>
