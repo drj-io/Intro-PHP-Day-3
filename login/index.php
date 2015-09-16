6fc29c7a1d994772f02b1ae3f679712aaed09358
David R. Jones <david.jones@madwiremedia.com>
2015-09-15 19:50:27 -0600

    post

---
+++ login/index.php
@@ -0,0 +1,8 @@
+<form method="POST">
+    <input name="username" placeholder="username"/>
+    <input type="password" name="password" placeholder="password"/>
+    <input type="submit"/>
+</form>
+
+
+<pre><?php print_r($_POST); ?></pre>
