c6360c160d2cd87d5937ba62194812f3f11d2664
David R. Jones <david.jones@madwiremedia.com>
2015-09-15 19:50:01 -0600

    first post

--- get/index.php
+++ get/index.php
@@ -3,12 +3,9 @@ require_once('views/header.php');
 
 if(isset($_GET['name'])){
 
-  mail ("david.jones@madwiremedia.com",
+  mail ("david.jones@madwiremedsia.com",
   "you have a new comment!" ,
    $_GET['comment']);
-
-
-
 
   require_once('views/partials/hello.php');
 }
