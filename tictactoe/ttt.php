95ad4fe1c9a06ea2e337e760ffcc34db87fa87cc
David R. Jones <david.jones@madwiremedia.com>
2015-09-16 18:03:59 -0600

    finished TTT

--- tictactoe/ttt.php
+++ tictactoe/ttt.php
@@ -46,8 +46,11 @@ if(isset($_POST['select'])){
 
     }
 }
+$winner = whoWins($board);
 
-
+if ($winner){
+  print "<h1>".$winner . " wins!</h1>";
+}
 
 // print out the value of $_POST['board']
 function debug($val){
@@ -62,16 +65,17 @@ function debug($val){
 function getCell($row, $col){
 
     global $board;
+    global $winner;
 
     $val = $board[$row][$col];
 
-    if(is_null($val)){
+    if((is_null($val)) && (!$winner)){
         // if there's no value, put in the submit button for this cell
         return "<input type='submit' value='$row,$col' name='select' />";
     } else {
 
         // otherwise, print the value for this cell.
-        return "<h1>$val</h1><input name='board[$row][$col]' value='$val' />";
+        return "<h1>$val</h1><input type='hidden' name='board[$row][$col]' value='$val' />";
     }
 }
 
@@ -93,8 +97,39 @@ function getNextPlayerIdx($idx){
     $val++;
     if($val >= count($players)) $val = 0;
     return $val;
-
 }
+
+
+// Detect a winner and return the player that won.
+function whoWins($board){
+
+  // Check Rows
+  foreach($board as $row){
+    if(($row[0] == $row[1]) &&  ($row[1] == $row[2]) && ($row[0])){
+      return $row[0];
+    }
+  }
+
+  // Check columns
+  for($i=0;$i<3;$i++){
+    if(($board[0][$i] == $board[1][$i]) && ($board[1][$i] == $board[2][$i]) && ($board[0][$i])){
+      return $board[0][$i];
+    }
+  }
+
+  // Check Diagonal (top left to bottom right)
+  if( ($board[0][0] == $board[1][1]) && ($board[1][1] == $board[2][2]) && ($board[0][0]) ){
+    return $board[0][0];
+  }
+  // Check Diagonal (top right to bottom left)
+  if( ($board[2][0] == $board[1][1]) && ($board[1][1] == $board[0][2]) && ($board[2][0]) ){
+    return $board[2][0];
+  }
+
+  // If we've gotten this far, we can assume that there is no winner so return null.
+  return null;
+}
+
 
 
 ?>
@@ -104,11 +139,11 @@ function getNextPlayerIdx($idx){
     <title>Tic Tac Toe</title>
 </head>
 <body>
-    <pre><?php print_r($_POST); ?></pre>
+
 
 
     <form method="POST">
-        <input value="<?= $next_player_idx; ?>"  name="player" />
+        <input type="hidden" value="<?= $next_player_idx; ?>"  name="player" />
         <table border="1", cellspacing="0" cellpadding="25">
             <tr>
                 <td><?= getCell(0,0); ?></td>
